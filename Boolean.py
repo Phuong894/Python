7c8d2759b70bfc311e1f028e2316140a2c2982be
Phuong894 <phuongissp2019@gmail.com>
2024-09-05 11:59:19 +0700

    Boolean.py

--- Boolean.py
+++ Boolean.py
@@ -1,15 +1,18 @@
 name = input('what is your name? ' )
 number = input(f"Hello {name}, please enter your identification number. ")
+#Ask for name and ID for some organization/company
 
 while True:
     verify = input(f'Kindly verify your identification number: {number}. Type Yes if it\'s correct. Otherwise, type No. ')
-
+#Verify if inputted value is true or false
     if verify.lower() == 'yes':
         break
+#If inputted yes it breaks the loop
     elif verify.lower() == 'no':
         number = input(f"Please re-enter your identification number. ")
     else:
         print("Invalid input. Please enter 'Yes' or 'No'.")
+#Loop repeats asking them for correct ID number
 
 print("Verification successful.")
 
